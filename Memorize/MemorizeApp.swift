1d2a9c53f9032ee69a1948199412fefa2e0cf517
Aleksandr Strokov <Aleksandr.Strokov@bki-okb.ru>
2022-08-03 21:41:42 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aleksandr Strokov on 03.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
